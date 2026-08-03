c11fec66bbf045602fad0e93abe3dc7ec8c25542
amanmathankar13 <mathankaraman13@gmail.com>
2026-08-04 01:08:53 +0530

    changes

--- Cosmetic Shop/force-app/main/default/lwc/registrationPage/registrationPage.js
+++ Cosmetic Shop/force-app/main/default/lwc/registrationPage/registrationPage.js
@@ -1,98 +1,67 @@
 import { LightningElement } from 'lwc';
-import registerCustomer from '@salesforce/apex/RegistrationController.registerCustomer';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class RegistrationPage extends LightningElement {
+import registerCustomer 
+from '@salesforce/apex/RegistrationController.registerCustomer';
 
-    firstName='';
-    lastName='';
-    email='';
-    phone='';
-    address='';
-    city='';
-    state='';
-    country='India';
-    pincode='';
-    password='';
-    confirmPassword='';
 
-    handleFirstName(event){ this.firstName=event.target.value; }
-    handleLastName(event){ this.lastName=event.target.value; }
-    handleEmail(event){ this.email=event.target.value; }
-    handlePhone(event){ this.phone=event.target.value; }
-    handleAddress(event){ this.address=event.target.value; }
-    handleCity(event){ this.city=event.target.value; }
-    handleState(event){ this.state=event.target.value; }
-    handleCountry(event){ this.country=event.target.value; }
-    handlePincode(event){ this.pincode=event.target.value; }
-    handlePassword(event){ this.password=event.target.value; }
-    handleConfirmPassword(event){ this.confirmPassword=event.target.value; }
+export default class CustomerRegistration extends LightningElement {
+
+
+    firstName;
+    lastName;
+    email;
+    phone;
+
+    message;
+
+
+
+    handleChange(event){
+
+        this[event.target.dataset.field]
+        =
+        event.target.value;
+
+    }
+
+
 
     register(){
 
-        if(this.password!==this.confirmPassword){
-
-            this.showToast(
-                'Error',
-                'Passwords do not match.',
-                'error'
-            );
-            return;
-        }
-
-        const registrationData={
-
-            firstName:this.firstName,
-            lastName:this.lastName,
-            email:this.email,
-            phone:this.phone,
-            address:this.address,
-            city:this.city,
-            state:this.state,
-            country:this.country,
-            pincode:this.pincode,
-            password:this.password
-
-        };
 
         registerCustomer({
 
-            registrationJson:JSON.stringify(registrationData)
+            firstName:this.firstName,
+
+            lastName:this.lastName,
+
+            email:this.email,
+
+            phone:this.phone
+
 
         })
-        .then(()=>{
 
-            this.showToast(
-                'Success',
-                'Registration Successful.',
-                'success'
-            );
+        .then(result=>{
 
-            window.location.href='/login';
+
+            this.message =
+            'Registration successful';
+
 
         })
+
         .catch(error=>{
 
-            this.showToast(
-                'Error',
-                error.body.message,
-                'error'
-            );
+
+            this.message =
+            error.body.message;
+
 
         });
 
-    }
-
-    showToast(title,message,variant){
-
-        this.dispatchEvent(
-            new ShowToastEvent({
-                title,
-                message,
-                variant
-            })
-        );
 
     }
+
 
 }
