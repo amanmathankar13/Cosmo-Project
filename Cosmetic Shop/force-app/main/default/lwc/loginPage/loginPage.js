dcba80104e7e263084dcc6d19a43416a49ef55ee
amanmathankar13 <mathankaraman13@gmail.com>
2026-08-04 03:00:02 +0530

    ee

--- Cosmetic Shop/force-app/main/default/lwc/loginPage/loginPage.js
+++ Cosmetic Shop/force-app/main/default/lwc/loginPage/loginPage.js
@@ -1,56 +1,43 @@
 import { LightningElement } from 'lwc';
-import login from '@salesforce/apex/LoginController.login';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class LoginPage extends LightningElement {
 
-    username = '';
-    password = '';
+export default class CustomerLogin extends NavigationMixin(LightningElement) {
 
-    handleUsername(event) {
+
+    username;
+    password;
+
+    errorMessage;
+
+
+
+    handleUsername(event){
+
         this.username = event.target.value;
+
     }
 
-    handlePassword(event) {
+
+
+    handlePassword(event){
+
         this.password = event.target.value;
-    }
-
-    async login() {
-
-        if (!this.username || !this.password) {
-
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Error',
-                    message: 'Please enter username and password.',
-                    variant: 'error'
-                })
-            );
-
-            return;
-        }
-
-        try {
-
-            const redirectUrl = await login({
-                username: this.username,
-                password: this.password
-            });
-
-            window.location.href = redirectUrl;
-
-        } catch (error) {
-
-            this.dispatchEvent(
-                new ShowToastEvent({
-                    title: 'Login Failed',
-                    message: error.body.message,
-                    variant: 'error'
-                })
-            );
-
-        }
 
     }
+
+
+
+    handleLogin(){
+
+
+        window.location.href =
+        '/servlet/servlet.su' +
+        '?email=' + this.username +
+        '&password=' + this.password;
+
+
+    }
+
 
 }
